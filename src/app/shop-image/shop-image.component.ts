47e4d492ce2b4518969265c61179a1d9890bdef8
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-12-18 00:36:37 +0100

    Working version of component and its example
    shop-image

--- src/app/shop-image/shop-image.component.ts
+++ src/app/shop-image/shop-image.component.ts
@@ -1,9 +1,10 @@
 import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'shop-image',
   template: `
-    <img #img (onLoad)="onImgLoad" (onError)="onImgError" alt="{{alt}}">
+    <img #img alt="{{alt}}">
   `,
   styles: [`
     :host {
@@ -12,6 +13,18 @@ import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChi
       overflow: hidden;
       background-size: cover;
       background-position: center;
+    }
+    img {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      max-width: 100%;
+      max-height: 100%;
+      margin: 0 auto;
+      opacity: 0;
+      transition: 0.5s opacity;
     }
   `]
 })
@@ -29,6 +42,8 @@ export class ShopImageComponent implements OnInit, OnChanges {
   constructor(private el: ElementRef) { }
 
   ngOnInit() {
+    this.img.nativeElement.addEventListener('load', this.onImgLoad.bind(this));
+    this.img.nativeElement.addEventListener('error', this.onImgError.bind(this));
   }
 
   onImgLoad(event) {
@@ -38,6 +53,7 @@ export class ShopImageComponent implements OnInit, OnChanges {
 
   onImgError(event) {
     if (!this.placeholderImg) {
+      // tslint:disable-next-line:max-line-length
       this.img.nativeElement.src = `data:image/svg+xml,${encodeURIComponent('<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#CCC" d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>')}`;
     }
   }
