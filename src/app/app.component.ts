a73ad396ad71a69f543a13155f4aadfc2b4e8540
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2016-12-18 23:09:48 +0100

    using team-card components in layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { TeamService } from './team.service';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'cmpnt-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'cmpnt works!';
+  teams: any[];
+
+  constructor(private teamService: TeamService) { }
+
+  ngOnInit(): void {
+    this.teamService
+      .get()
+      .subscribe((teams: any[]) => {
+        this.teams = teams;
+      })
+  }
 }
